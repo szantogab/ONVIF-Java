6e8fc662a29fec5308f6735aeb75da6d7c87c546
Gabor <>
2022-06-05 12:01:02 +0200

    Update to 1.1.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,14 +11,14 @@ repositories {
 }
 
 group = "be.teletask.onvif"
-version = "1.1.12"
+version = "1.1.13"
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains", "annotations", "15.0")
     implementation("net.sf.kxml", "kxml2", "2.3.0")
     implementation("com.squareup.okhttp3", "okhttp", "4.9.3")
-    implementation("com.burgstaller", "okhttp-digest", "2.0")
+    implementation("io.github.rburgst", "okhttp-digest", "2.7")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.4")
 }
 
