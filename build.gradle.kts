ed4dcc89321a2195fe23fe52d3c553bff344075f
Gabor <>
2022-06-05 12:06:25 +0200

    Build gradle null fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/szantogab/ONVIF-Java")
             credentials {
-                username = project.findProperty("gpr.user") as String ?: System.getenv("GITHUB_USERNAME")
-                password = project.findProperty("gpr.key") as String ?: System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
             }
         }
     }
